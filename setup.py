2f869ebffa9d88bbd19fd67bc71939b6767a589a
OsmanAdiguzel <58969628+OsmanAdiguzel@users.noreply.github.com>
2021-07-05 17:33:41 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  2 19:46:13 2021
+
+@author: oadiguzel
+"""
+from setuptools import setup
+setup(
+      name="mytest",
+      version="0.0.1",
+      description="for test",
+      py_modules=["functions","greet"],
+      package_dir={"":"mypackage"},
+      url=""
+      author="Osman Adiguzel",
+      authot_email="o.adiguzel008@gmail.com"
+      )
