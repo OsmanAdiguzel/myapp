0cea27bab54742ad129c4903322d8933726ac7f3
OsmanAdiguzel <58969628+OsmanAdiguzel@users.noreply.github.com>
2021-07-05 22:08:24 +0300

    Add files via upload

---
+++ mypackage/functions.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  2 19:46:27 2021
+
+@author: oadiguzel
+"""
+
+def sum(x,y):
+    return x+y
+
+def average(x,y):
+    return (x+y)/2
+
+def power(x,y):
+    return x**y
---
+++ mypackage/greet.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  2 19:46:13 2021
+
+@author: oadiguzel
+"""
+def SayHello(name):
+    print("Hello ", name)
